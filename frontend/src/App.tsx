7b97a986aad38878021fa2d61c17f6c140f82b13
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2024-09-02 14:50:53 +0200

    feat: Added Welcome Page and Solver Page (routing not working yet)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,36 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useTranslation } from 'react-i18next';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import WelcomePage from "@/components/pages/WelcomePage.tsx";
+import SolverPage from "@/components/pages/SolverPage.tsx";
 function App() {
-  const [count, setCount] = useState(0)
-  const { t, i18n } = useTranslation();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      
-      <h1>{t('Welcome to React')}</h1>
-    </>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />}>
+            <Route index element={<SolverPage />} />
+            <Route path="solver" element={<SolverPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
 }
+
 
 export default App
