a50352271af03788ed1527a0f933248640e38c1e
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2024-09-02 15:20:02 +0200

    fix: package.json fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import WelcomePage from "@/components/pages/WelcomePage.tsx";
 import SolverPage from "@/components/pages/SolverPage.tsx";
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
               <Route path="/" element={<WelcomePage />} /> {/* Root route */}
               <Route path="/solver" element={<SolverPage />} /> {/* /solver route */}
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
