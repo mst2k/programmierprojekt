e8419607a4fb8d28e3bede25f2bb3bbb1018b802
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2024-09-02 14:58:05 +0200

    fix: Routing is now Working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { useTranslation } from 'react-i18next';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WelcomePage from "@/components/pages/WelcomePage.tsx";
 import SolverPage from "@/components/pages/SolverPage.tsx";
 function App() {
   return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<WelcomePage />}>
-            <Route index element={<SolverPage />} />
-            <Route path="solver" element={<SolverPage />} />
-          </Route>
-        </Routes>
+          <Routes>
+              <Route path="/" element={<WelcomePage />} /> {/* Root route */}
+              <Route path="/solver" element={<SolverPage />} /> {/* /solver route */}
+          </Routes>
       </BrowserRouter>
   );
 }
