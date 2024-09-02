8e56bd075356caf9d7be71a80152b01702396c8b
mst2k <marcel.strohm@proton.me>
2024-09-02 14:12:55 +0200

    trigger pipeline

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
