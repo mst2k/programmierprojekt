b3b6b3993d90a1227f9388c241d9f1cef25fb1eb
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2024-09-17 12:04:31 +0200

    Merge remote-tracking branch 'origin/14-solverpage-implementieren' into 14-solverpage-implementieren

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
