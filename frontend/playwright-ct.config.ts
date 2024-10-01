fc87d97f00db063025b4345f05156ad0d2fa9ba0
22marcel11 <marcel.strohm@web.de>
2024-10-01 15:30:56 +0000

    fix(ci): adjust e2e pipeline

--- frontend/playwright-ct.config.ts
+++ frontend/playwright-ct.config.ts
@@ -28,6 +28,13 @@ export default defineConfig({
     ctPort: 3100,
   },
 
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
+
   /* Configure projects for major browsers */
   projects: [
     {
