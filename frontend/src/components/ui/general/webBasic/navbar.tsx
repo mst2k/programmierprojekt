8c0bad9a225a9cd311053178fe8f3828bc4dbf21
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2024-10-07 15:24:18 +0200

    fix(darkMode) dark Mode in Mobile View

--- frontend/src/components/ui/general/webBasic/navbar.tsx
+++ frontend/src/components/ui/general/webBasic/navbar.tsx
@@ -12,21 +12,27 @@ import { useNavigate } from "react-router-dom"
 import { cn } from "@/lib/utils"
 import Logo from '/logo.svg'
 import { useTranslation } from "react-i18next"
-import { Menu } from 'lucide-react'
-import { Button } from "@/components/ui/button.tsx"
+import { Menu, Sun, Moon } from 'lucide-react'
+import { Button } from "@/components/ui/button"
 import {
     Sheet,
     SheetContent,
     SheetTrigger,
-} from "@/components/ui/sheet.tsx"
+} from "@/components/ui/sheet"
+import { useTheme } from "@/components/ui/general/themeProvider"
 
 export function NavigationMenuDemo() {
     const navigate = useNavigate();
     const { t, i18n } = useTranslation();
     const [isOpen, setIsOpen] = useState(false);
+    const { theme, setTheme } = useTheme();
 
     const changeLanguage = (lng: string) => {
         i18n.changeLanguage(lng);
+    }
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark": "light")
     }
 
     const components: { title: string; lang: string; description: string }[] = [
@@ -43,7 +49,7 @@ export function NavigationMenuDemo() {
     ]
 
     return (
-        <div className="w-full flex justify-between items-center px-4 py-0 my-1">
+        <div className="w-full flex justify-between items-center px-4 py-0 my-1 bg-background dark:bg-background-dark">
             <NavigationMenu className={"md:hidden"}>
                 <NavigationMenuList>
                     <NavigationMenuItem>
@@ -64,14 +70,14 @@ export function NavigationMenuDemo() {
                                 <img src={Logo} className="logo" alt="Logo"></img> </NavigationMenuLink>
                         </NavigationMenuItem>
                         <NavigationMenuItem>
-                            <NavigationMenuSingleTrigger onClick={()=> navigate('/')}>{t('title')}</NavigationMenuSingleTrigger> {/* menue-item title*/}
-                        </NavigationMenuItem>{ /*TODO: Nach demo wieder Aktivieren */}
+                            <NavigationMenuSingleTrigger className="bg-background  " onClick={()=> navigate('/')}>{t('title')}</NavigationMenuSingleTrigger> {/* menue-item title*/}
+                        </NavigationMenuItem>
                         <NavigationMenuItem>
-                            <NavigationMenuSingleTrigger onClick={() => navigate('/converter/')}> {t('converter')}</NavigationMenuSingleTrigger>
+                            <NavigationMenuSingleTrigger className="bg-background  " onClick={() => navigate('/converter/')}> {t('converter')}</NavigationMenuSingleTrigger>
                         </NavigationMenuItem>
 
                         <NavigationMenuItem >
-                            <NavigationMenuSingleTrigger onClick={()=> navigate('/solver/')}>{t('solver')}</NavigationMenuSingleTrigger>
+                            <NavigationMenuSingleTrigger className="bg-background  " onClick={()=> navigate('/solver/')}>{t('solver')}</NavigationMenuSingleTrigger>
                         </NavigationMenuItem>
                     </NavigationMenuList>
                 </NavigationMenu >
@@ -81,14 +87,21 @@ export function NavigationMenuDemo() {
                     <NavigationMenuList>
                         {/* right elements */}
                         <NavigationMenuItem >
-                            <NavigationMenuSingleTrigger onClick={()=> navigate('/about/')}>{t('about')}</NavigationMenuSingleTrigger>
+                            <NavigationMenuSingleTrigger className="bg-background  " onClick={()=> navigate('/about/')}>{t('about')}</NavigationMenuSingleTrigger>
                         </NavigationMenuItem>
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger>{t('language')}</NavigationMenuTrigger> {/* menue-item title*/}
+                            <Button variant="ghost" size="icon" className="bg-background  " onClick={() => { toggleTheme(); setIsOpen(false); }}>
+                                {theme === 'dark' ? <Sun className="h-[1.2rem] w-[1.2rem]" /> : <Moon className="h-[1.2rem] w-[1.2rem]" />}
+                                {/* <span className="sr-only">Toggle theme</span> */}
+                            </Button>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger className="bg-background" >{t('language')}</NavigationMenuTrigger> {/* menue-item title   |  dark:bg-background-dark dark:text-foreground-dark*/}
                             <NavigationMenuContent>
                                 <ul className="grid w-[200px] gap-3 p-4 md:w-[150] lg:w-[300px]">
                                     {components.map((component) => (
                                         <ListItem
+                                        className = "dark:text-foreground-dark"
                                             key={component.title}
                                             title={component.title}
                                             onClick={() => changeLanguage(component.lang)}  //type-conversion to ensure only setting-values
@@ -126,12 +139,15 @@ export function NavigationMenuDemo() {
                             <Button variant="ghost" className="w-full justify-start" onClick={() => { navigate('/about/'); setIsOpen(false); }}>
                                 {t('about')}
                             </Button>
+                            <Button variant="ghost" className="w-full justify-start" onClick={() => { toggleTheme(); setIsOpen(false); }}>
+                                {theme === 'dark' ? <Sun className="h-[1.2rem] w-[1.2rem]" /> : <Moon className="h-[1.2rem] w-[1.2rem]" />}
+                            </Button>
                             <NavigationMenu>
                                 <NavigationMenuList>
                                     <NavigationMenuItem>
-                                        <NavigationMenuTrigger>{t('language')}</NavigationMenuTrigger>
+                                        <NavigationMenuTrigger className="bg-background  ">{t("language")}</NavigationMenuTrigger>
                                         <NavigationMenuContent>
-                                            <ul className="grid w-[200px] gap-3 p-4">
+                                            <ul className="grid w-[200px] gap-3 p-4 bg-background  ">
                                                 {components.map((component) => (
                                                     <ListItem
                                                         key={component.title}
@@ -167,13 +183,15 @@ const ListItem = React.forwardRef<
                 <a
                     ref={ref}
                     className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors",
+                        "hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        "dark:text-foreground-dark dark:hover:bg-accent dark:hover:text-accent-foreground",
                         className
                     )}
                     {...props}
                 >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                    <div className="text-sm font-medium leading-none dark:text-foreground-dark">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground dark:text-muted-foreground-dark">
                         {children}
                     </p>
                 </a>
