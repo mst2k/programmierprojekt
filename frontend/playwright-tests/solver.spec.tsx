9b12e3351c9352d28f333f999158c662c249f19a
22marcel11 <marcel.strohm@web.de>
2024-10-09 06:01:05 +0000

    fix: remove faulty test

--- frontend/playwright-tests/solver.spec.tsx
+++ frontend/playwright-tests/solver.spec.tsx
@@ -11,51 +11,6 @@ test('Open solver page and solve transportation problem', async ({ page }) => {
   await expect(page.locator('#root')).toContainText('Zielfunktionswert: 153.675');
 });
 
-test('input transport problem and copy link', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  await page.getByRole('button', { name: 'Löser' }).click();
-  await page.getByRole('button', { name: 'Einfach' }).click();
-  await page.getByRole('button', { name: 'Transport' }).click();
-  await page.getByPlaceholder('Werksname').click();
-  await page.getByPlaceholder('Werksname').fill('a');
-  await page.getByPlaceholder('Kapazität').click();
-  await page.getByPlaceholder('Kapazität').fill('5');
-  await page.locator('div').filter({ hasText: /^Produktionswerke$/ }).getByRole('button').click();
-  await page.getByPlaceholder('Werksname').nth(1).click();
-  await page.getByPlaceholder('Werksname').nth(1).fill('b');
-  await page.getByPlaceholder('Kapazität').nth(1).click();
-  await page.getByPlaceholder('Kapazität').nth(1).fill('10');
-  await page.getByPlaceholder('Marktname').click();
-  await page.getByPlaceholder('Marktname').fill('c');
-  await page.getByPlaceholder('Nachfrage').click();
-  await page.getByPlaceholder('Nachfrage').fill('3');
-  await page.locator('.p-4 > div:nth-child(3) > button').click();
-  await page.getByPlaceholder('Marktname').nth(1).click();
-  await page.getByPlaceholder('Marktname').nth(1).fill('d');
-  await page.getByPlaceholder('Nachfrage').nth(1).click();
-  await page.getByPlaceholder('Nachfrage').nth(1).fill('12');
-  await page.getByPlaceholder('Entfernungen').first().click();
-  await page.getByPlaceholder('Entfernungen').first().fill('2');
-  await page.getByPlaceholder('Entfernungen').nth(1).click();
-  await page.getByPlaceholder('Entfernungen').nth(1).fill('3');
-  await page.getByPlaceholder('Entfernungen').nth(2).click();
-  await page.getByPlaceholder('Entfernungen').nth(2).fill('4');
-  await page.getByPlaceholder('Entfernungen').nth(3).click();
-  await page.getByPlaceholder('Entfernungen').nth(3).fill('5');
-  await page.getByPlaceholder('Frachtkosten (Pro Einheit und').click();
-  await page.getByPlaceholder('Frachtkosten (Pro Einheit und').fill('3');
-  await page.getByRole('button', { name: 'Lösen' }).click();
-  await expect(page.locator('#root')).toContainText('GLPKHgourvest GLPK Solver Ergebnis');
-  await page.getByText('Zielfunktionswert:').click();
-  await expect(page.locator('#root')).toContainText('Zielfunktionswert: 0.186');
-  await page.locator('div').filter({ hasText: /^LösenGMPL-Code anzeigen$/ }).locator('div').getByRole('button').click();
-  await page.getByRole('button', { name: 'Kopieren' }).click();
-  await page.locator('div').filter({ hasText: /^Kopieren$/ }).getByRole('textbox').press('ControlOrMeta+c');
-  await page.getByRole('button', { name: 'Schließen' }).click();
-});
-
-
-
 test('Open shareable link and assert values, solve and check result', async ({ page }) => {
   await page.goto('http://localhost:5173//#/solver?param=H4sIAAAAAAAACn2NQQ7CIBBFT0PXSiWsWNj2AF5hhLEShTbDdOHtBTW4MLiZ%2BcnL%2B3%2B9Q%2BRkhBqEHoSUEQLmJ%2FpjvlCSHPO1sIL1%2FKhIlaSnQn%2B9c9vb7z6imroAdMPmuK0lDgNEV4H%2BN%2B1a1uG763xiiBbfy6p09FWTJalXe4Pkhguhn688LomN7OxGhJFPMKNhgpjWhfgJdkMqmFkBAAA%3D');
   await expect(page.getByPlaceholder('Werksname').first()).toHaveValue('a');
